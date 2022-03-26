f1384c9b8cf6cdba09c32d06795df81b17a4ba14
mary-puma <pumaisabel029@gmail.com>
2022-03-26 00:08:03 -0300

    excepciones personalizadas y salidas de codigos http en spring boot personalizadas

---
+++ src/main/java/com/sistema/blog/excepciones/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.sistema.blog.excepciones;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetalles> manejarResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
+        ErrorDetalles errorDetalles = new ErrorDetalles(new Date(),exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetalles, HttpStatus.NOT_FOUND);
+    }
+
+}
